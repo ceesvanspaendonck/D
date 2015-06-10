f714d2826138dfae47a99433077efb5fb48a8571
maxnoon <maxturk@msn.com>
2015-06-10 13:18:02 +0200

    tja hitler didnt do anything wrong weetje

--- src/d/Spel.java
+++ src/d/Spel.java
@@ -2,6 +2,8 @@ package d;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
 
 /**
@@ -12,12 +14,46 @@ import java.io.FileNotFoundException;
 public class Spel extends JPanel
 {
     public static JLabel stappen;
+    static JFrame frame = new JFrame("Best Doolhof Ever");
+    static PressListener listener ;
+    
+    
     
     public static void main(String[] args) throws FileNotFoundException
     {
-        JFrame frame = new JFrame("Best Doolhof Ever");
+        Speler speler = new Speler(1, 6);
+        Bazooka bazooka = new Bazooka(1, 1);  
         frame.setVisible(true);
        
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+       
+        Doolhof doolhofP = new Doolhof(15, 15);
+       
+
+        
+        stappen = new JLabel("Aantal stappen: " + Doolhof.stappen,SwingConstants.CENTER);
+        stappen.setPreferredSize(new Dimension(300, 100));
+        stappen.setVisible(true);
+        listener = new PressListener(speler);
+        frame.addKeyListener(listener);
+        frame.getContentPane().add(stappen, BorderLayout.SOUTH);
+        Drawing drawing = new Drawing();
+        //speler.move("right");
+        frame.add(drawing);
+        frame.setSize(400,600);
+   
+    }
+
+
+    public static void restart() throws FileNotFoundException{
+        stappen.setText(""); // reset de stappen
+    
+        Spel.frame.removeKeyListener(listener);
+        Spel.frame.dispose();
+        Spel.frame.setVisible(false);
+      
+        
+        frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         Doolhof doolhofP = new Doolhof(15, 15);
@@ -29,13 +65,13 @@ public class Spel extends JPanel
         stappen = new JLabel("Aantal stappen: " + Doolhof.stappen,SwingConstants.CENTER);
         stappen.setPreferredSize(new Dimension(300, 100));
         stappen.setVisible(true);
-        PressListener listener = new PressListener(speler);
+        
         frame.addKeyListener(listener);
         frame.getContentPane().add(stappen, BorderLayout.SOUTH);
         Drawing drawing = new Drawing();
- 
+       // speler.move("right");
         frame.add(drawing);
-               frame.setSize(400,600);
+        frame.setSize(400,600);
+       
     }
-
 }
